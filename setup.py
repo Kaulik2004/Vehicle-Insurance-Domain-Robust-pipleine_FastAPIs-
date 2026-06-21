27909d2ffc0d1812ab4e07fddc107b688d4d911c
Sunny <kaulikdas2017@gmail.com>
2026-06-21 13:42:57 +0530

    logging and exception added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Kaulik Das",
+    author_email="kaulikdas2017@gmail.com",
+    packages=find_packages()
+)
